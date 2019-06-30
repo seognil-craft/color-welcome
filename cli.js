12a42a908106688f67ce514c2fdde6d2bc0c5928
seognil LC <seognil@gmail.com>
2019-06-30 18:42:20 +0800

    refactor: replace dep by @darkobits/lolcatjs

--- cli.js
+++ cli.js
@@ -1,21 +1,24 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
-const lolcatjs = require('lolcatjs');
+const Printer = require('@darkobits/lolcatjs').default;
 
-lolcatjs.options.seed = ~~(Math.random() * 1024);
-// lolcatjs.options.seed = 36;
-lolcatjs.options.spread = 8;
-lolcatjs.options.freq = 0.1;
+// Math.sin(freq * i + (N * 2 * Math.PI) / 3) * 127 + 128;
+
+const opts = {
+  //   seed: Math.random() * 960 * Math.PI,
+  spread: 8,
+  freq: 0.1,
+};
 
 const theWelcome = String.raw`
-   _______   _______   _______   _______   _     _   _______   _       
-  /  ____ \ /  ____ \ /  ___  \ /  ____ \ | \   | | |__   __| | |      
-  | |    \/ | |    \/ | |   | | | |    \/ |  \  | |    | |    | |      
-  | |_____  | |__     | |   | | | |       |   \ | |    | |    | |      
-  |_____  | |  __|    | |   | | | | ____  | |\ \| |    | |    | |      
-        | | | |       | |   | | | | \_  | | | \   |    | |    | |      
+   _______   _______   _______   _______   _     _   _______   _
+  /  ____ \ /  ____ \ /  ___  \ /  ____ \ | \   | | |__   __| | |
+  | |    \/ | |    \/ | |   | | | |    \/ |  \  | |    | |    | |
+  | |_____  | |__     | |   | | | |       |   \ | |    | |    | |
+  |_____  | |  __|    | |   | | | | ____  | |\ \| |    | |    | |
+        | | | |       | |   | | | | \_  | | | \   |    | |    | |
   /\____| | | |____/\ | |___| | | |___| | | |  \  |  __| |__  | |____/\
   \_______/ \_______/ \_______/ \_______/ |_|   \_| |_______| |_______/
 `;
 
-lolcatjs.fromString(theWelcome);
+console.warn(Printer.fromString(theWelcome, opts));
